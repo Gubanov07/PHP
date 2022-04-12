db24a3cb2198680d755dce4902d650c9be6f515f
Gubanov Alekcandr <sgubanov325@gmail.com>
2022-04-12 21:42:49 +0700

    10

--- 123/main.php
+++ 123/main.php
@@ -1,37 +1,41 @@
-<?php
-if(isset($_REQUEST['submit']) && !empty($_REQUEST['text'])){
-$text = $_REQUEST['text'];
-$length = mb_strlen($text,'UTF-8');
-$arr = [];
-for($i=0;$i < $length;$i++){
-$char = mb_substr($text,$i,1,'UTF-8');
-if(!isset($arr[$char])){
-$arr[$char] = 1;
-} else{
-$arr[$char]++;
-}
-}
-foreach($arr as $key=>$elem){
-$percent = round($elem * 100 / $length, 0);
-echo "Процентное содержание '$key'".' в тексте - '.$percent.'%<br>';
-}
-}
-if(isset($_REQUEST['submit']) && empty($_REQUEST['text'])){
-echo 'Введите текст в   форму';
+ <?php
+$month = [1 => 'январь', 'февраль', 'март', 'апрель', 'май', 'июнь', 'июль', 'август', 'сентябрь', 'октябрь', 'ноябрь', 'декабрь'];
+$week = ['воскресенье', 'понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота'];
+if (isset($_REQUEST)) {
+    $day = $_REQUEST['day'];
+    $mon = array_search($_REQUEST['month'], $month);
+    $year = $_REQUEST['year'];
+    $dayOfWeek = $week[date('w', mktime(0, 0, 0, $mon, $day, $year))];
+    echo $dayOfWeek;
 }
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <form action="main.php" method="GET" align="center">
-<p>процент символов в тексте</p>
-<textarea rows="5" cols="50" name="text"><?php if(isset($_REQUEST['submit'])) echo $_REQUEST['text']; ?></textarea><br><br>
-<input type="submit" name="submit" value="Подсчитать">
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+     <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>Document</title>
+ </head>
+ <body>
+    <form action="main.php" method="post">
+  <textarea name="text" placeholder="">hello my friend!
+</textarea>
+    <select name="day">
+        <?php for ($i = 1; $i <= 31; $i++): ?>
+            <option><?=$i;?></option>
+        <?php endfor;?>
+    </select>
+    <select name="month">
+        <?php foreach ($month as $item): ?>
+            <option><?=$item;?></option>
+        <?php endforeach;?>
+    </select>
+    <select name="year">
+        <?php for ($i = 1990; $i <= 2025; $i++): ?>
+            <option><?=$i;?></option>
+        <?php endfor;?>
+    </select>
+    <input type="submit">
 </form>
 </body>
 </html> 
