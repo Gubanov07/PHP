905b3337f3fd90d6fda2a39fa9cfbf97040e70da
Gubanov Alekcandr <sgubanov325@gmail.com>
2022-04-26 17:45:12 +0700

    94.1

--- 0423/project/config/routes.php
+++ 0423/project/config/routes.php
@@ -5,5 +5,6 @@
 		new Route('/my-page1/', 'test', 'act1'),
 		new Route('/my-page2/', 'test', 'act2'),
 		new Route('/my-page3/', 'test', 'act3'),
+		new Route('/nums/:n1/:n2/:n3/', 'num', 'sum'),
 	];
 ?>
