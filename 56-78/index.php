29647e3c4f799ec9c6d78a01e17f8248e360d83a
Gubanov Alekcandr <sgubanov325@gmail.com>
2022-05-23 11:03:42 +0700

    57

--- 56-78/index.php
+++ 56-78/index.php
@@ -1,24 +1,14 @@
-<?php 
+<?php
 require_once 'date.php';
-    $date = new Date('2025-12-31');
+require_once 'interval.php';
 
-    echo $date->getYear(); echo ' ';  // выведет '2025'
-    echo $date->getMonth(); echo ' '; // выведет '12'
-    echo $date->getDay(); echo ' '; // выведет '31' 
-
-    echo '<br>';
-
-    echo $date->getWeekDay(); echo ' ';    // выведет '3'
-    echo $date->getWeekDay('ru'); echo ' '; // выведет 'среда'
-    echo $date->getWeekDay('en'); echo ' '; // выведет 'wednesday'
-
-    echo '<br>';
-
-    echo (new Date('2025-12-31'))->addYear(1); // '2026-12-31'
-    echo '<br>';
-    echo (new Date('2025-12-31'))->addDay(1);  // '2026-01-01'
-    echo '<br>';
-    echo (new Date('2025-12-31'))->subDay(3); // '2025-12-28'
-    echo '<br>';
-    echo (new Date('2025-12-28'))->addYear(1); // '2026-12-28'
-?>
+    $date1 = new Date('2025-12-31');
+    $date2 = new Date('2026-11-28');
+    
+    $interval = new Interval($date1, $date2);
+    
+    echo $interval->toDays();   // выведет разницу в днях
+    echo $interval->toMonths(); // выведет разницу в месяцах
+    echo $interval->toYears();  // выведет разницу в годах
+    
+?> 
