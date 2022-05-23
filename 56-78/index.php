578eedd03cee9e4cfe04819ad90ad038e1b2a1f4
Gubanov Alekcandr <sgubanov325@gmail.com>
2022-05-23 11:22:37 +0700

    59.3

--- 56-78/index.php
+++ 56-78/index.php
@@ -4,4 +4,6 @@ require_once 'tag.php';
 $img = new Tag('img');
     echo $img->open().'img'.$img->close(); 
 
+$header = new Tag('header');
+    echo $header->open().'header сайта'.$header->close();
 ?> 
