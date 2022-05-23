81e869a8ce2d5635b7670a7600a283779bdf8188
Gubanov Alekcandr <sgubanov325@gmail.com>
2022-05-23 11:32:13 +0700

    61.1

--- 56-78/Tag.php
+++ 56-78/Tag.php
@@ -1,38 +1,43 @@
 <?php
-    class Tag
+class Tag
     {
         private $name;
+        private $attrs = [];
 
         public function __construct($name)
         {
             $this->name = $name;
         }
-
+        
         public function open()
         {
             $name = $this->name;
             return "<$name>";
         }
-
+        
         public function close()
         {
             $name = $this->name;
             return "</$name>";
         }
-
         private function getAttrsStr($attrs)
     {
         if (!empty($attrs)) {
             $result = '';
-
+            
             foreach ($attrs as $name => $value) {
                 $result .= " $name=\"$value\"";
             }
-
+            
             return $result;
         } else {
             return '';
         }
-    }
-}
+    } 
+    public function setAttr($name, $value)
+        {
+            $this->attrs[$name] = $value;
+            return $this; 
+        } 
+    } 
 ?> 
