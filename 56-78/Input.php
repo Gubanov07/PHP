4fc7d6f377b7ca658033ef1a96bcb0df4d65d2c9
Gubanov Alekcandr <sgubanov325@gmail.com>
2022-05-26 11:35:54 +0700

    72.1

---
+++ 56-78/Input.php
@@ -0,0 +1,10 @@
+<?php
+require_once 'Tag.php';
+    class Input extends Tag
+    {
+        public function __construct()
+        {
+            parent::__construct('input');
+        }
+    }
+?>
--- 56-78/index.php
+++ 56-78/index.php
@@ -1,7 +1,13 @@
 <?php
+require_once 'Input.php';
 require_once 'Form.php';
-$form = (new Form)->setAttrs(['action' => 'test.php', 'method' => 'POST']);
-echo $form->open();
-        
+    $form = (new Form)->setAttrs(['action' => '', 'method' 
+        => 'GET']); 
+    
+    echo $form->open();
+        echo (new Input)->setAttr('name', 'year')->
+            open(); 
+        echo (new Input)->setAttr('type', 'submit')->
+            open(); 
     echo $form->close();
 ?>
