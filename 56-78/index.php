8860ca83fb3a7542356c005df127ddaf719ff0bd
Gubanov Alekcandr <sgubanov325@gmail.com>
2022-05-26 12:01:17 +0700

    75

--- 56-78/index.php
+++ 56-78/index.php
@@ -2,13 +2,13 @@
 require_once 'Input.php';
 require_once 'Form.php';
 require_once 'Submit.php';
-require_once 'Password.php'
+require_once 'Hidden.php';
 
 $form = (new Form)->setAttrs(['action' => 'test.php', 'method' => 'GET']);
 
-echo $form->open();
-echo (new Input)   ->setAttr('name', 'login');
-echo (new Password)->setAttr('name', 'passw');
-echo new Submit;
-echo $form->close();
+    echo $form->open();
+        echo (new Hidden)->setAttr('name', 'id')->setAttr('value', '123'); 
+        echo (new Input)->setAttr('name', 'year');
+        echo new Submit;
+    echo $form->close();
 ?>
