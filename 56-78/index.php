4abfb1c77d1492b2babbbd5d5a93374e05020155
Gubanov Alekcandr <sgubanov325@gmail.com>
2022-05-26 11:19:43 +0700

    70.3

--- 56-78/index.php
+++ 56-78/index.php
@@ -1,8 +1,19 @@
 <?php
 require_once 'HtmlList.php';
-    $list = new HtmlList('ul');
+require_once 'ClassUl.php';
+require_once 'ClassOl.php';
+    $ul = new Ul;
+    $ol = new Ol;
 
-    echo $list
+    echo $ol
+        ->addItem((new ListItem())->setText('item1'))
+        ->addItem((new ListItem())->setText('item2'))
+        ->addItem((new ListItem())->setText('item3'))
+        ->show(); 
+
+echo '<br>';
+    echo $ul
+
         ->addItem((new ListItem())->setText('item1'))
         ->addItem((new ListItem())->setText('item2'))
         ->addItem((new ListItem())->setText('item3'))
