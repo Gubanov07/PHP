dd30d771328cf2005054193443d98c55dfe4c7b1
Gubanov Alekcandr <sgubanov325@gmail.com>
2022-05-26 11:01:38 +0700

    69. 1-5

---
+++ 56-78/menu.php
@@ -0,0 +1,8 @@
+<?php
+require_once 'Link.php';
+    $link1 = (new Link())->setAttr('href', '/56-78/1.php')->setText('First');
+    $link2 = (new Link())->setAttr('href', '/56-78/2.php')->setText('Second');
+    $link3 = (new Link())->setAttr('href', '/56-78/3.php')->setText('Third');
+    $link4 = (new Link())->setAttr('href', '/56-78/4.php')->setText('Fourth');
+    $link5 = (new Link())->setAttr('href', '/56-78/5.php')->setText('Fifth');
+?>
