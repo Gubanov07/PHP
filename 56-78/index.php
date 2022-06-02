f74f6c97b63530246c853cd3dbeb859a9dfc1bd7
Gubanov Alekcandr <sgubanov325@gmail.com>
2022-06-02 08:44:43 +0700

    77.1

--- 56-78/index.php
+++ 56-78/index.php
@@ -2,13 +2,13 @@
 require_once 'Input.php';
 require_once 'Form.php';
 require_once 'Submit.php';
-require_once 'Textarea.php';
+require_once 'Checkbox.php';
 
 $form = (new Form)->setAttrs(['action' => 'test.php', 'method' => 'GET']);
 
     echo $form->open();
+        echo (new Checkbox)->setAttr('name', 'checkbox');
         echo (new Input)->setAttr('name', 'user');
-        echo (new Textarea)->setAttr('name', 'message')->show();
         echo new Submit;
     echo $form->close();
 ?>
